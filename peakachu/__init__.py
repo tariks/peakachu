c8bf781d582c2485d35f45b83d3945f22d96a1de
Tarik Salameh <tariksalameh@gmail.com>
2019-02-22 10:47:10 -0500

    resource contrib version

--- peakachu/__init__.py
+++ peakachu/__init__.py
@@ -1,6 +1,6 @@
 # Author: Tarik Salameh
 
 __author__ = 'Tarik Salameh'
-__version__ = '0.0.19'
+__version__ = '0.1.0'
 __license__ = 'GPLv3+'
 Me = __file__
--- peakachu/call_loops.py
+++ peakachu/call_loops.py
@@ -17,29 +17,24 @@ def main(args):
         c = X[:,1].astype(int)//res
         p = X[:,2].astype(float)
         raw = X[:,3].astype(float)
-        idx=(p>.8)
-        r,c,p,raw = r[idx],c[idx],p[idx],raw[idx]
-        maxy = np.max(c)+1
-        rawmatrix=np.zeros((maxy,maxy))
-        matrix=np.zeros((maxy,maxy))
-        matrix[r,c]+=p
-        rawmatrix[r,c]+=raw
+        rawmatrix={(r[i],c[i]): raw[i] for i in range(len(r))}
+        matrix={(r[i],c[i]): p[i] for i in range(len(r))}
         del X
         gc.collect()
         idx = np.argsort(p)
         idx = idx[-10000:]
-        D = {(r[i],c[i]): raw[i] for i in idx}
+        D = {(r[i],c[i]): raw.get((r[i],c[i])) for i in idx}
         final_list = peakacluster.local_clustering(D,res=res)
         final_list = [i[0] for i in final_list]
         r = [i[0] for i in final_list]
         c = [i[1] for i in final_list]
+        p = np.array([matrix.get((r[i],c[i])) for i in range(len(r))])
         if len(r) > 1000:
-            p=matrix[r,c]
             sorted_index=np.argsort(p)
             r = [r[i] for i in sorted_index[-1000:]]
             c = [c[i] for i in sorted_index[-1000:]]
         for i in range(len(r)):
-            P=matrix[r[i],c[i]]
+            P = matrix.get((r[i],c[i]))
             print(chrom,r[i]*res,r[i]*res+res,chrom,
                   c[i]*res,c[i]*res+res,
                   P,sep='\t')
