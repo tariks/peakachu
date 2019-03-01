d7edaf7d1377c23897294726cabf4cd2a114c19d
Tarik Salameh <tariksalameh@gmail.com>
2019-03-01 08:01:11 -0500

    report reads 20kb-3Mb

--- peakachu/calculate_depth.py
+++ peakachu/calculate_depth.py
@@ -4,16 +4,21 @@ import pathlib
 def main(args):
     import argparse, sys
     import numpy as np
+    from scipy import sparse
     np.seterr(divide='ignore',invalid='ignore')
     import cooler
     Lib = cooler.Cooler(args.path)
     #resolution = Lib.binsize
     resolution = 10000
     sums=0
+    totals=0
     for k in Lib.chromnames[:]:
-        x=Lib.matrix(balance=False,sparse=False).fetch(k)
-        sums+=np.sum(np.triu(x))
+        x=Lib.matrix(balance=False,sparse=False).fetch(k).tocsr()
+        sums+=sparse.triu(x,k=2).sum()
+        sums-=sparse.triu(x,k=300).sum()
+        totals+=sparse.triu(x).sum()
 
-    print('processed {} counts'.format(sums))
+    print(sums)
+    print(totals)
 
 
