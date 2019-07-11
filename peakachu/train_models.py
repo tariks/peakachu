61582983d860477eac49a8ad7117b18e55a79356
Tarik Salameh <tariksalameh@gmail.com>
2019-07-10 21:24:14 -0400

    Revert "changed training input to require negative file"
    This reverts commit 5e94086c5622e634e1fc9284af34f1c3f8f14f28.

--- peakachu/train_models.py
+++ peakachu/train_models.py
@@ -14,8 +14,7 @@ def main(args):
     Lib = cooler.Cooler(args.path)
     #resolution = Lib.binsize
     resolution = args.resolution
-    coords = trainUtils.parsebed(args.bedpe,lower=1,res=resolution)
-    negcoords = trainUtils.parsebed(args.control,lower=1,res=resolution)
+    coords = trainUtils.parsebed(args.bedpe,lower=2,res=resolution)
     # train model per chromosome
     positive_class = {}
     negative_class = {}
@@ -31,14 +30,18 @@ def main(args):
         R,C,data = R[validmask],C[validmask],X.data[validmask]
         X = csr_matrix((data, (R, C)), shape=X.shape)
         del data
+        idx = (C-R > 20000//resolution) & (C-R < 160000//resolution)
+        R,C = R[idx],C[idx]
         clist = coords[chromname]
-        nlist = negcoords[chromname]
         try:
             positive_class[chromname] = np.vstack((f for f in trainUtils.buildmatrix(
                                              X,clist,width=args.width)))
-            stop=positive_class[chromname].shape[0]
-            negative_class[chromname] = np.vstack((f for f in trainUtils.buildmatrix(
-                                             X,nlist,stop=stop,positive=False,width=args.width)))
+            neg_coords = [(r,c) for r,c in zip(R,C)]
+            random.shuffle(neg_coords)
+            stop = 9*len(clist)//10
+            negative_class[chromname]=np.vstack((f for f in trainUtils.buildmatrix(
+                             X,neg_coords[::5],width=args.width,
+                             positive=False,stop=stop)))
         except:
             print(chromname, ' failed to gather fts')
 
