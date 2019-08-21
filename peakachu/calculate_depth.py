782d1e42107e19b911be47af8bc110b3587ef6c4
Xiaotao Wang <wangxiaotao686@gmail.com>
2019-08-20 21:51:53 -0500

    Update calculate_depth.py

--- peakachu/calculate_depth.py
+++ peakachu/calculate_depth.py
@@ -8,17 +8,12 @@ def main(args):
     np.seterr(divide='ignore',invalid='ignore')
     import cooler
     Lib = cooler.Cooler(args.path)
-    #resolution = Lib.binsize
-    resolution = 10000
-    sums=0
+    
     totals=0
     for k in Lib.chromnames[:]:
-        x=Lib.matrix(balance=False,sparse=False).fetch(k).tocsr()
-        sums+=sparse.triu(x,k=2).sum()
-        sums-=sparse.triu(x,k=300).sum()
-        totals+=sparse.triu(x).sum()
-
-    print(sums)
+        intra = np.triu(lib.matrix(balance=False, sparse=False).fetch(k))
+        totals += int(intra.sum())
+    
     print(totals)
 
 
