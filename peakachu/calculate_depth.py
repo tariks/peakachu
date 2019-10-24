43d44b06cc834d9ffbcf006fa3ef711dac61c70d
zhongquan <zhongquan789@126.com>
2019-10-24 18:10:17 +0800

    Impreove readability; Pep8 style.

--- peakachu/calculate_depth.py
+++ peakachu/calculate_depth.py
@@ -1,24 +1,26 @@
 
 def main(args):
 
-    import argparse, sys
+    import argparse
+    import sys
     import numpy as np
     from scipy import sparse
     from peakachu import utils
 
-    np.seterr(divide='ignore',invalid='ignore')
-    
-    check = utils.read_hic_header(args.path) # more robust to check if a file is .hic
+    np.seterr(divide='ignore', invalid='ignore')
+
+    # more robust to check if a file is .hic
+    check = utils.read_hic_header(args.path)
     if check is None:
         hic = False
     else:
         hic = True
 
-    totals=0
+    totals = 0
     if not hic:
         import cooler
         Lib = cooler.Cooler(args.path)
-    
+
         for k in Lib.chromnames[:]:
             intra = np.triu(Lib.matrix(balance=False, sparse=False).fetch(k))
             totals += int(intra.sum())
@@ -28,10 +30,8 @@ def main(args):
         for k in hic_info['chromsizes']:
             try:
                 intra = straw.straw('NONE', args.path, k, k, 'BP', 10000)
-                totals += sum(intra[2]) # intra is a list of list x, y, v
+                totals += sum(intra[2])  # intra is a list of list x, y, v
             except:
-                pass # handle the inconsistency between .hic header and the matrix
+                pass  # handle the inconsistency between .hic header and the matrix
 
     print(totals)
-
-
--- peakachu/score_chromosome.py
+++ peakachu/score_chromosome.py
@@ -1,23 +1,28 @@
 #!/usr/bin/env python
 
+
 def main(args):
 
-    import argparse, gc, pathlib, os
+    import argparse
+    import gc
+    import pathlib
+    import os
     import numpy as np
     from sklearn.externals import joblib
     from peakachu import scoreUtils, utils
 
-    np.seterr(divide='ignore',invalid='ignore')
-    
-    pathlib.Path(args.output).mkdir(parents=True, exist_ok=True) 
+    np.seterr(divide='ignore', invalid='ignore')
+
+    pathlib.Path(args.output).mkdir(parents=True, exist_ok=True)
 
     model = joblib.load(args.model)
 
-    hic_info = utils.read_hic_header(args.path) # more robust to check if a file is .hic
+    # more robust to check if a file is .hic
+    hic_info = utils.read_hic_header(args.path)
     if hic_info is None:
-        hic=False
+        hic = False
     else:
-        hic=True
+        hic = True
 
     if not hic:
         import cooler
@@ -25,31 +30,31 @@ def main(args):
         chromosomes = Lib.chromnames[:]
     else:
         chromosomes = utils.get_hic_chromosomes(args.path, args.resolution)
-    
+
     pre = utils.find_chrom_pre(chromosomes)
-    tmp = os.path.split(args.model)[1] # support full path
-    ccname = pre + tmp.split('.pk')[0].lstrip('chr') # ccname is consistent with chromosome labels in .hic / .cool
-    cikada = 'chr' + ccname.lstrip('chr') # cikada always has prefix "chr"
-    
+    tmp = os.path.split(args.model)[1]  # support full path
+    # ccname is consistent with chromosome labels in .hic / .cool
+    ccname = pre + tmp.split('.pk')[0].lstrip('chr')
+    cikada = 'chr' + ccname.lstrip('chr')  # cikada always has prefix "chr"
+
     if not hic:
         X = scoreUtils.Chromosome(Lib.matrix(balance=args.balance, sparse=True).fetch(ccname).tocsr(),
-                                model=model,
-                                cname=cikada,lower=args.lower,
-                                upper=args.upper,res=args.resolution,
-                                width=args.width)
+                                  model=model,
+                                  cname=cikada, lower=args.lower,
+                                  upper=args.upper, res=args.resolution,
+                                  width=args.width)
     else:
         if args.balance:
-            X = scoreUtils.Chromosome(utils.csr_contact_matrix('KR',args.path,ccname,ccname,'BP',args.resolution),
-                                model=model,
-                                cname=cikada,lower=args.lower,
-                                upper=args.upper,res=args.resolution,
-                                width=args.width)
+            X = scoreUtils.Chromosome(utils.csr_contact_matrix('KR', args.path, ccname, ccname, 'BP', args.resolution),
+                                      model=model,
+                                      cname=cikada, lower=args.lower,
+                                      upper=args.upper, res=args.resolution,
+                                      width=args.width)
         else:
-            X = scoreUtils.Chromosome(utils.csr_contact_matrix('NONE',args.path,ccname,ccname,'BP',args.resolution),
-                                model=model,
-                                cname=cikada,lower=args.lower,
-                                upper=args.upper,res=args.resolution,
-                                width=args.width)
-    result,R = X.score()
-    X.writeBed(args.output,result,R)
-
+            X = scoreUtils.Chromosome(utils.csr_contact_matrix('NONE', args.path, ccname, ccname, 'BP', args.resolution),
+                                      model=model,
+                                      cname=cikada, lower=args.lower,
+                                      upper=args.upper, res=args.resolution,
+                                      width=args.width)
+    result, R = X.score()
+    X.writeBed(args.output, result, R)
