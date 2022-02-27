b5e19970296161da75345c3f0a3d7c32e5b2ad00
Xiaotao Wang <wangxiaotao686@gmail.com>
2022-02-26 22:07:30 -0600

    new release

--- peakachu/call_loops.py
+++ peakachu/call_loops.py
@@ -12,14 +12,15 @@ def main(args):
     res = args.resolution
     
     clusters, score_pool = peakacluster.parse_peakachu(args.infile, args.threshold, res)
-    for c in clusters:
-        for p in clusters[c]:
-            if p in score_pool[c]:
-                s1 = str(p[0]*res)
-                e1 = str(p[0]*res+res)
-                s2 = str(p[1]*res)
-                e2 = str(p[1]*res+res)
-                prob = str(score_pool[c][p][0])
-                raw_signal = str(score_pool[c][p][1])
-                line = [c, s1, e1, c, s2, e2, prob, raw_signal]
-                out.write('\t'.join(line)+'\n')
+    with open(args.outfile, 'w') as out:
+        for c in clusters:
+            for p in clusters[c]:
+                if p in score_pool[c]:
+                    s1 = str(p[0]*res)
+                    e1 = str(p[0]*res+res)
+                    s2 = str(p[1]*res)
+                    e2 = str(p[1]*res+res)
+                    prob = str(score_pool[c][p][0])
+                    raw_signal = str(score_pool[c][p][1])
+                    line = [c, s1, e1, c, s2, e2, prob, raw_signal]
+                    out.write('\t'.join(line)+'\n')
