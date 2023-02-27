63c18f4d3882f2a20a8f1ddffab2de6f1e6b5537
Xiaotao Wang <wangxiaotao686@gmail.com>
2023-02-27 14:34:40 +0800

    update README

--- peakachu/utils.py
+++ peakachu/utils.py
@@ -1,7 +1,4 @@
-
-# Read information from the hic header
-
-import struct, io, os, straw
+import os, hicstraw
 import numpy as np
 from sklearn.isotonic import IsotonicRegression
 from scipy.sparse import csr_matrix
@@ -63,73 +60,25 @@ def find_chrom_pre(chromlabels):
     else:
         return ''
 
-
-def readcstr(f):
-    buf = ""
-    while True:
-        b = f.read(1)
-        b = b.decode('utf-8', 'backslashreplace')
-        if b is None or b == '\0':
-            return str(buf)
-        else:
-            buf = buf + b
-
-
 def read_hic_header(hicfile):
 
     if not os.path.exists(hicfile):
-        return None  # probably a cool URI
-
-    req = open(hicfile, 'rb')
-    magic_string = struct.unpack('<3s', req.read(3))[0]
-    req.read(1)
-    if (magic_string != b"HIC"):
-        return None  # this is not a valid .hic file
-
+        return None
+    
+    hic = hicstraw.HiCFile(hicfile)
     info = {}
-    version = struct.unpack('<i', req.read(4))[0]
-    info['version'] = str(version)
+    info['Genome ID'] = hic.getGenomeID()
 
-    masterindex = struct.unpack('<q', req.read(8))[0]
-    info['Master index'] = str(masterindex)
-
-    genome = ""
-    c = req.read(1).decode("utf-8")
-    while (c != '\0'):
-        genome += c
-        c = req.read(1).decode("utf-8")
-    info['Genome ID'] = str(genome)
-
-    nattributes = struct.unpack('<i', req.read(4))[0]
-    attrs = {}
-    for i in range(nattributes):
-        key = readcstr(req)
-        value = readcstr(req)
-        attrs[key] = value
-    info['Attributes'] = attrs
-
-    nChrs = struct.unpack('<i', req.read(4))[0]
     chromsizes = {}
-    for i in range(nChrs):
-        name = readcstr(req)
-        length = struct.unpack('<i', req.read(4))[0]
-        if name != 'ALL':
+    for chrom_obj in hic.getChromosomes():
+        name = chrom_obj.name
+        length = chrom_obj.length
+        if (name.upper() != 'ALL'):
             chromsizes[name] = length
 
     info['chromsizes'] = chromsizes
-
-    info['Base pair-delimited resolutions'] = []
-    nBpRes = struct.unpack('<i', req.read(4))[0]
-    for i in range(nBpRes):
-        res = struct.unpack('<i', req.read(4))[0]
-        info['Base pair-delimited resolutions'].append(res)
-
-    info['Fragment-delimited resolutions'] = []
-    nFrag = struct.unpack('<i', req.read(4))[0]
-    for i in range(nFrag):
-        res = struct.unpack('<i', req.read(4))[0]
-        info['Fragment-delimited resolutions'].append(res)
-
+    info['resolutions'] = hic.getResolutions()
+    
     return info
 
 def calculate_expected(M, maxdis, raw=False):
